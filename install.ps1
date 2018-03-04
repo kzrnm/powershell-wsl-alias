112acd632fbe87aa69bc487f5fc0ddb23bc71a3e
naminodarie <gengesa@gmail.com>
2018-03-05 00:46:10 +0900

    define command by alias

--- install.ps1
+++ install.ps1
@@ -2,49 +2,59 @@
 
 $modulePath = ($env:PSModulePath -split ";")[0] + '\wsl-alias\wsl-alias.psm1'
 $funcTemplate = @'
-function $name {
+function \$wslcommand {
     if($MyInvocation.ExpectingInput){
-        $input | & wsl $name $color $args
+        $input | & wsl $MyInvocation.InvocationName $args
     }else{
-        & wsl $name $color $args
+        & wsl $MyInvocation.InvocationName $args
+    }
+}
+function \$wslcolorcommand {
+    if($MyInvocation.ExpectingInput){
+        $input | & wsl $MyInvocation.InvocationName --color=auto $args
+    }else{
+        & wsl $MyInvocation.InvocationName --color=auto $args
     }
 }
 '@
-
-if($convertPath) {
-    $funcTemplate = $funcTemplate.Replace('$args','($args | ForEach-Object{ToWslPath $_})')
-}
-
 $ToWslPathFunction = @'
 function ToWslPath ($path) {
     if (-not (Test-Path $path)) { return $path }
     if (Split-Path $path -IsAbsolute) {
-        $path = "/mnt/" + (Split-Path $path -Qualifier).Replace(":", "").ToLower()        + (Split-Path $path -NoQualifier)
+        $path = "/mnt/" + (Split-Path $path -Qualifier).Replace(":", "").ToLower() + (Split-Path $path -NoQualifier)
     }
     return $path.Replace("\", "/")
 }
 '@
 
+Remove-Module wsl-alias -Force 2>$null
 Set-Location $PSScriptRoot
 New-Item $modulePath -Force
 
-$functions = [System.Collections.Generic.List[string]]::new()
-$functions.Add($ToWslPathFunction)
+if ($convertPath) {
+    $ToWslPathFunction | Out-File $modulePath -Encoding ascii -Append
+    $funcTemplate = $funcTemplate.Replace('$args', '($args | ForEach-Object{ToWslPath $_})')
+}
+$funcTemplate | Out-File $modulePath -Encoding ascii -Append
 
 $commands = if ($whiteList) {$whiteList} else {& wsl sh get_command.sh}
 $commands = $commands | Where-Object { $blackList -notcontains $_ }
+if (-not $overrideDefault) { $commands = $commands | Where-Object { -not(Test-Path alias:$_) } }
+
+$setAliasCommands = [System.Collections.Generic.List[string]]::new()
 foreach ($name in $commands) {
     if ($autoColorCommands -contains $name) {
-        $color = '--color=auto'
-    } else {
-        $color = ''
+        $setAliasCommands.Add('Set-Alias {0} ''\$wslcolorcommand''' -f $name)
+    }
+    else {
+        $setAliasCommands.Add('Set-Alias {0} ''\$wslcommand''' -f $name)
     }
 
-    $func = $funcTemplate.Replace('$name', $name).Replace('$color', $color)
-    $functions.Add($func)
     Write-Host $name
 }
-$functions | Out-File $modulePath -Encoding ascii
-"Export-ModuleMember -Function $($commands -join ',')" | Out-File $modulePath -Encoding ascii -Append
+$setAliasCommands | Out-File $modulePath -Encoding ascii -Append
+
+'Export-ModuleMember -Function ''\$wslcommand'',''\$wslcolorcommand''' | Out-File $modulePath -Encoding ascii -Append
+"Export-ModuleMember -Alias $($commands -join ',')" | Out-File $modulePath -Encoding ascii -Append
 
 Import-Module wsl-alias
